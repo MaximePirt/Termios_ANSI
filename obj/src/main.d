91938af96ace1276f21d7bc7ed3272776be52b5e
CleSucre <47931504+CleSucre@users.noreply.github.com>
2024-10-06 21:35:37 +0200

    .gitognore added

--- obj/src/main.d
+++
@@ -1,6 +0,0 @@
-obj/src/main.o: src/main.c include/minishell.h include/struct.h \
- include/minishell.h include/struct.h
-include/minishell.h:
-include/struct.h:
-include/minishell.h:
-include/struct.h:
